3ec809404f4fcc3f735f6e44ceb4b821a7ca1d2f
W41K3R0 <mahapatrarik123@gmail.com>
2023-10-04 22:04:57 +0530

    updated

--- index.js
+++ index.js
@@ -1,51 +1,77 @@
-const http = require('http');
-const fs = require('fs');
-const querystring = require('querystring'); // Import the querystring module
-const port = 3000; // Set your desired port
+const express = require('express');
+const bodyParser = require('body-parser');
+const { google } = require('googleapis');
+const { auth } = require('google-auth-library');
 
-process.chdir("https://github.com/W41K3R0/waitingList")
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'POST' && req.url === '/add-email') {
-        let body = '';
+app.use(bodyParser.urlencoded({ extended: true }));
 
-        req.on('data', (data) => {
-            body += data;
+// Replace with your Google Docs API credentials file
+const credentials = require('./waiting-list-401015-45c81134bfa4.json');
+
+// Define the Google Docs document ID where you want to write the email IDs
+const documentId = '1G_fR1_wgP-0NfhqV9yWQakmi3UvJNV2r5q6OupK4r9Y';
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', async (req, res) => {
+    const email = req.body.email;
+
+    try {
+        const client = await auth.getClient({
+            credentials,
+            scopes: ['https://www.googleapis.com/auth/documents'],
         });
 
-        req.on('end', () => {
-            // Parse the URL-encoded data
-            const parsedData = querystring.parse(body);
-            const email = parsedData.email;
+        const docs = google.docs({ version: 'v1', auth });
 
-            if (email) {
-                fs.appendFile('./waitlist.txt', email + '\n', (err) => {
-                    if (err) {
-                        console.error(err);
-                        res.statusCode = 500;
-                        res.end('Error saving email.');
-                    } else {
-                        res.statusCode = 200;
-                        res.end('Email added to the waitlist.');
-                    }
-                });
-            } else {
-                res.statusCode = 400;
-                res.end('Invalid email address.');
-            }
+        // Retrieve the current content of the document
+        const response = await docs.documents.get({ documentId });
+
+        const document = response.data;
+        const currentContent = document.body.content;
+
+        // Create a new paragraph with the submitted email
+        const newContent = [
+            ...currentContent,
+            { paragraph: { elements: [{ textRun: { content: email } }] } },
+        ];
+
+        // Update the document content with the new content
+        await docs.documents.batchUpdate({
+            documentId,
+            resource: {
+                requests: [
+                    {
+                        replaceAllText: {
+                            containsText: {
+                                text: '{{EMAIL}}',
+                                matchCase: true,
+                            },
+                            replaceText: email,
+                        },
+                    },
+                    {
+                        replaceContent: {
+                            target: { textRange: { type: 'ALL' } },
+                            content: newContent,
+                        },
+                    },
+                ],
+            },
         });
-    } else {
-        res.statusCode = 404;
-        res.end('Not Found');
+
+        res.send('Email ID submitted successfully!');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error submitting email ID.');
     }
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
